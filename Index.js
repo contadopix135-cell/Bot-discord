3400f4f9ab6aa3f1ccc9816a988a791fd3c4b72a
contadopix135-cell <contadopix135@gmail.com>
2026-06-27 10:32:40 -0300

    Index.js

---
+++ Index.js
@@ -0,0 +1,25 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+const prefix = "A!";
+
+client.on("ready", () => {
+  console.log(`✅ Logado como ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === prefix + "saldo") {
+    message.reply("💰 Seu saldo é 0");
+  }
+});
+
+client.login(process.env.TOKEN);
